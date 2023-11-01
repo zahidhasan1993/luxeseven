4d30bd5735409d8e91aa41159af2eb2913a9a42b
devZahid <officialzahidhasan19932021@gmail.com>
2023-11-01 13:39:14 +0600

    adding gallery to the project

--- clint/src/components/home/Gallery.jsx
+++ clint/src/components/home/Gallery.jsx
@@ -1,55 +1,67 @@
+import gallery1 from "../../assets/gallery/1.avif";
+import gallery2 from "../../assets/gallery/2.avif";
+import gallery3 from "../../assets/gallery/3.avif";
+import gallery4 from "../../assets/gallery/4.avif";
+import gallery5 from "../../assets/gallery/5.avif";
+import gallery6 from "../../assets/gallery/6.avif";
+import gallery7 from "../../assets/gallery/7.avif";
+import gallery8 from "../../assets/gallery/8.avif";
+import gallery9 from "../../assets/gallery/9.avif";
+import gallery10 from "../../assets/gallery/10.avif";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+
 const Gallery = () => {
   return (
     <section className="dark:bg-gray-800 dark:text-gray-50 mb-10">
       <div className="container grid grid-cols-2 gap-4 p-4 mx-auto md:grid-cols-4">
-        <img
-          src="https://source.unsplash.com/random/301x301/"
-          alt=""
+        <LazyLoadImage
+          src={gallery1}
+          alt="Gallery Image"
           className="w-full h-full col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-3 md:row-start-1 dark:bg-gray-500 aspect-square"
         />
-        <img
-          alt=""
+        <LazyLoadImage
+          alt="Gallery Image"
           className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square"
-          src="https://source.unsplash.com/random/200x200/?0"
+          src={gallery2}
         />
-        <img
-          alt=""
+        <LazyLoadImage
+          alt="Gallery Image"
           className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square"
-          src="https://source.unsplash.com/random/200x200/?1"
+          src={gallery3}
         />
-        <img
-          alt=""
+        <LazyLoadImage
+          alt="Gallery Image"
           className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square"
-          src="https://source.unsplash.com/random/200x200/?2"
+          src={gallery4}
         />
-        <img
-          alt=""
+        <LazyLoadImage
+          alt="Gallery Image"
           className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square"
-          src="https://source.unsplash.com/random/200x200/?3"
+          src={gallery5}
         />
-        <img
-          alt=""
+        <LazyLoadImage
+          alt="Gallery Image"
           className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square"
-          src="https://source.unsplash.com/random/200x200/?4"
+          src={gallery6}
         />
-        <img
-          alt=""
+        <LazyLoadImage
+          alt="Gallery Image"
           className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square"
-          src="https://source.unsplash.com/random/200x200/?5"
+          src={gallery7}
         />
-        <img
-          alt=""
+        <LazyLoadImage
+          alt="Gallery Image"
           className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square"
-          src="https://source.unsplash.com/random/200x200/?6"
+          src={gallery8}
         />
-        <img
-          alt=""
+        <LazyLoadImage
+          alt="Gallery Image"
           className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square"
-          src="https://source.unsplash.com/random/200x200/?7"
+          src={gallery9}
         />
-        <img
-          src="https://source.unsplash.com/random/302x302/"
-          alt=""
+        <LazyLoadImage
+          src={gallery10}
+          alt="Gallery Image"
           className="w-full h-full col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-1 md:row-start-3 dark:bg-gray-500 aspect-square"
         />
       </div>
