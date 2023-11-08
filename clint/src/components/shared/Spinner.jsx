75647a931010363fa882736394215542dfe18f02
devZahid <officialzahidhasan19932021@gmail.com>
2023-11-08 14:37:17 +0600

    styling the spinner

--- clint/src/components/shared/Spinner.jsx
+++ clint/src/components/shared/Spinner.jsx
@@ -1,11 +1,7 @@
 const Spinner = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div
-        style={{ borderTopColor: "transparent" }}
-        className="w-8 h-8 border-4 border-blue-200 rounded-full animate-spin"
-      ></div>
-      <p className="ml-2">cargando...</p>
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="w-20 h-20 rounded-full animate-spin border-x-8 border-solid border-black border-t-transparent"></div>
     </div>
   );
 };
