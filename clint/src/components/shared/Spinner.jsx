40a44cd03f0aeae7f61528c0e75afa73334460cd
devZahid <officialzahidhasan19932021@gmail.com>
2023-11-08 14:42:24 +0600

    adding react spinner to the project

--- clint/src/components/shared/Spinner.jsx
+++ clint/src/components/shared/Spinner.jsx
@@ -1,7 +1,9 @@
+import ClockLoader from "react-spinners/ClockLoader";
+
 const Spinner = () => {
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <div className="w-20 h-20 rounded-full animate-spin border-x-8 border-solid border-black border-t-transparent"></div>
+      <ClockLoader color={"#000000"} size={150} />
     </div>
   );
 };
