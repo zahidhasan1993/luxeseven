e414089d1152b5856d4aec389fcd2513ef342852
devZahid <officialzahidhasan19932021@gmail.com>
2023-11-12 14:35:13 +0600

    adding antd and moment to the project to pickup the booking date from the use

--- clint/src/components/rooms/Rooms.jsx
+++ clint/src/components/rooms/Rooms.jsx
@@ -1,14 +1,29 @@
 import useRooms from "../../customhooks/useRooms";
 import RoomCard from "../shared/RoomCard";
+import { DatePicker } from "antd";
+import moment from 'moment';
+
+const { RangePicker } = DatePicker;
 
 const Rooms = () => {
   const rooms = useRooms();
-  console.log(rooms);
+  // console.log(rooms);
+  const filterByDate = (dates) => {
+    const startDate = moment(dates[0]?.$d).format('DD-MM-YYYY');
+    const endDate = moment(dates[1]?.$d).format('DD-MM-YYYY');
+    
+    
+  }
   return (
     <div className="my-20">
-      {rooms.map((room) => (
-        <RoomCard key={room._id} item={room}></RoomCard>
-      ))}
+      <div className="md:p-10">
+        <RangePicker format="DD-MM-YYYY" onChange={filterByDate} className="border border-black h-12"/>
+      </div>
+      <div>
+        {rooms.map((room) => (
+          <RoomCard key={room._id} item={room}></RoomCard>
+        ))}
+      </div>
     </div>
   );
 };
