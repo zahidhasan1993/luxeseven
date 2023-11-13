443b5165c5cc6db02c45a9f921bd970fd1256778
devZahid <officialzahidhasan19932021@gmail.com>
2023-11-13 14:16:13 +0600

    adding booking details page and share data

--- clint/src/components/rooms/BookingDetails.jsx
+++ clint/src/components/rooms/BookingDetails.jsx
@@ -2,11 +2,17 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 import { useLoaderData } from "react-router-dom";
 import { BookingProvider } from "../../providers/DateProvider";
 import { useContext } from "react";
+import moment from "moment";
 
 const BookingDetails = () => {
   const item = useLoaderData().data;
   const { checkIn, checkOut } = useContext(BookingProvider);
-  console.log(checkIn, checkOut);
+  // console.log(item);
+
+  const startMoment = moment(checkIn, "DD-MM-YYYY");
+  const endMoment = moment(checkOut, "DD-MM-YYYY");
+  const differenceInDays = endMoment.diff(startMoment, "days");
+  const totalRent = differenceInDays * item.rent;
   return (
     <div className="my-20 md:flex md:gap-10">
       <div className="md:w-1/2">
@@ -26,15 +32,34 @@ const BookingDetails = () => {
         </div>
       </div>
       <div className="text-gray-600  text-2xl space-y-5 md:w-1/2">
-        <p className="">Name: {item.name}</p>
-        <p>Max Count : {item.maxCount}</p>
-        <p>Phone : {item.phone}</p>
-        <p>Type : {item.type}</p>
-        <p>Rent per Day : ${item.rent}</p>
-
-        <p>Check In : {checkIn}</p>
-        <p>Check Out : {checkOut}</p>
-
+        <p className="font-agbalumo">
+          Name: <span className="font-serif">{item.name}</span>
+        </p>
+        <p className="font-agbalumo">
+          Max Count: <span className="font-serif">{item.maxCount}</span>
+        </p>{" "}
+        <p className="font-agbalumo">
+          Phone: <span className="font-serif">{item.phone}</span>
+        </p>{" "}
+        <p className="font-agbalumo">
+          Type: <span className="font-serif">{item.type}</span>
+        </p>{" "}
+        <p className="font-agbalumo">
+          Check In: <span className="font-serif">{checkIn}</span>
+        </p>{" "}
+        <p className="font-agbalumo">
+          Check Out: <span className="font-serif">{checkOut}</span>
+        </p>
+        <hr className="divide-y divide-black" />
+        <p className="font-agbalumo">
+          Rent Per Day: <span className="font-serif">{item.rent}</span>
+        </p>
+        <p className="font-agbalumo">
+          Total Days: <span className="font-serif">{differenceInDays}</span>
+        </p>
+        <p className="font-agbalumo">
+          Total Amount: <span className="font-serif">${totalRent}</span>
+        </p>
         <button className="bg-black py-2 w-full text-white hover:scale-105 duration-300 ease-linear hover:bg-white hover:text-black hover:font-agbalumo">
           Pay
         </button>
