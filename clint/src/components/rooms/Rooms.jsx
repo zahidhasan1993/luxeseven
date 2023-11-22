d532d185d3734204cac9c4ecf3f61c8c44562db2
devZahid <officialzahidhasan19932021@gmail.com>
2023-11-22 14:42:51 +0600

    adding search by room type

--- clint/src/components/rooms/Rooms.jsx
+++ clint/src/components/rooms/Rooms.jsx
@@ -44,15 +44,41 @@ const Rooms = () => {
   };
   // console.log(checkIn,checkOut);
 
+  const filterByType = (e) => {
+    const type = e.target.value;
+    const byType = fakeRooms.filter(
+      (room) => room.type.toLowerCase() === type.toLowerCase()
+    );
+    console.log(byType);
+    setNewRooms(byType);
+  };
   return (
     <div className="my-20">
-      <div className="md:p-10 mb-10">
-        <RangePicker
-          format="DD-MM-YYYY"
-          onChange={filterByDate}
-          className="border border-black h-12 w-full"
-        />
+      <div className="md:flex md:justify-between md:items-center mb-10">
+        <div className="md:p-10 w-full">
+          <RangePicker
+            format="DD-MM-YYYY"
+            onChange={filterByDate}
+            className="border border-black h-12 w-full"
+          />
+        </div>
+
+        <div className="mt-5 md:mt-0 w-full">
+          <select
+            onClick={filterByType}
+            className="h-12 bg-gray-50 border border-black text-gray-500 text-lg rounded block w-full p-2.5"
+          >
+            <option defaultValue="all">Choose room type</option>
+            <option defaultValue="delux" className="text-black">
+              Delux
+            </option>
+            <option defaultValue="non-delux" className="text-black">
+              Non-Delux
+            </option>
+          </select>
+        </div>
       </div>
+
       <div>
         {}
         {newRooms.length === 0
